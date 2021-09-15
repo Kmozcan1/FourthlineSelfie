68a3e32da5c343840640144630ac3c381cbcc4cb
Kadir Mert Özcan <kmozcan1@gmail.com>
2021-09-15 18:20:18 +0200

    Initial commit

---
+++ app/src/main/java/com/fourthline/assignment/presentation/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fourthline.assignment.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fourthline.assignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
